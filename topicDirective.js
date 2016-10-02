077b4363ae3e207e38c435f226ffb9960ca47bf8
mhorton19 <hmark2@umbc.edu>
2016-10-01 23:58:17 -0400

    Create topicDirective.js

---
+++ topicDirective.js
@@ -0,0 +1,27 @@
+angular.module('treeOfKnowledge').directive('topicDir', function($compile) {
+    return {
+        restrict: 'E',
+//        template: '<h1>penis</h1>'
+        replace: 'true',
+        templateUrl: 'topic.html',
+        controller: 'topicController',
+        controllerAs: 'vm',
+        bindToController: 'true',
+        scope: {
+            topic: '='
+        },
+        //lol wut
+        compile: function(tElement, tAttr) {
+            var contents = tElement.contents().remove();
+            var compiledContents;
+            return function(scope, iElement, iAttr) {
+                if(!compiledContents) {
+                    compiledContents = $compile(contents);
+                }
+                compiledContents(scope, function(clone, scope) {
+                         iElement.append(clone); 
+                });
+            };
+        }
+    };
+});
