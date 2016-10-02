d9d5dcff5a2b077326f67534d80151ba4f619ca7
zjones3 <zjones3@umbc.edu>
2016-10-02 00:52:54 -0400

    Update topicController.js

--- topicController.js
+++ topicController.js
@@ -1,19 +1,29 @@
-angular.module('treeOfKnowledge').controller('topicController', function($scope) {
+angular.module('treeOfKnowledge').controller('topicController', function ($scope) {
     var vm = this;
-    
+
     vm.topic = $scope.topic;
     vm.init = init;
-    vm.name = $scope.topic.name;
-    vm.penis = 'penis';
-    
-    init();
-    
-    function init(){
-        console.log("in directive");
-        console.log(vm);
-    }
-    
-    
-    
-});
+    vm.draw = draw;
 
+    init();
+
+    function init() {
+
+    }
+
+    function draw() {
+        var c = document.getElementById("lineCanvas");
+        var rect = document.getElementById("canvasRow").getBoundingClientRect();
+        c.width = rect.width;
+        c.height = rect.height;
+        var ctx = c.getContext("2d");
+        ctx.moveTo(0, 0);
+        ctx.lineTo(rect.width, rect.height);
+        ctx.stroke();
+    }
+
+
+
+
+
+});
